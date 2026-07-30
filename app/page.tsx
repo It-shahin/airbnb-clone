aa650d2c8f21fd70d517afd289fbb12be81fffc4
Chahin Boudra <boudrachahin@gmail.com>
2026-07-30 20:40:39 +0100

    feat: implement listings retrieval and display with empty state handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,47 @@
-export default function Home() {
+import ClientOnly from "@/components/ClientOnly";
+import Container from "@/components/container";
+import EmptyState from "@/components/EmptyState";
+import getListings from "./actions/getListings";
+import ListingCard from "@/components/listings/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
+
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    )
+  }
   return (
     <div>
-      <h1 className="text-2xl text-rose-500">Airbnb</h1>
+      <ClientOnly>
+        <Container>
+          <div className="
+            pt-24
+            grid
+            grid-cols-1
+            sm:grid-cols-2
+            md:grid-cols-3
+            lg:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          ">
+            {listings.map((listing: any) => {
+              return (
+                <ListingCard
+                  currentUser={currentUser as any}
+                  key={listing.id}
+                  data={listing}
+                />
+              )
+            })}
+          </div>
+        </Container>
+      </ClientOnly>
     </div>
   );
 }
